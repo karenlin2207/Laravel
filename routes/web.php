e11b9b15b66be9b43d2bac504bcb31419daf2930
Ubuntu <ubuntu@ip-172-31-29-134.ap-southeast-1.compute.internal>
2017-08-10 07:22:48 +0000

    merge

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', 'HomeController@index');
 
@@ -23,45 +23,45 @@ Route::get('/products/{product}', 'ProductController@detail')->name('products.de
 Route::get('/products', 'ProductController@list')->name('products.list');
 
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
-	Route::group(['prefix' => 'products'], function(){
-	Route::get('/', function(){
-		return view('product.index');
-	})->name('products.index');
-	Route::post('/', 'ProductController@store')->name('products.store');
-	Route::get('create', 'ProductController@create')->name('products.create');
-	Route::get('{product}', 'ProductController@show')->name('products.show');
-	Route::get('edit/{product}', 'ProductController@edit')->name('products.edit');
-	Route::put('{product}', 'ProductController@update')->name('products.update');
-	Route::delete('{product}', 'ProductController@delete')->name('products.delete');
-	});	
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+	Route::group(['prefix' => 'products'], function () {
+		Route::get('/', function () {
+			return view('product.index');
+		})->name('products.index');
+		Route::post('/', 'ProductController@store')->name('products.store');
+		Route::get('create', 'ProductController@create')->name('products.create');
+		Route::get('{product}', 'ProductController@show')->name('products.show');
+		Route::get('edit/{product}', 'ProductController@edit')->name('products.edit');
+		Route::put('{product}', 'ProductController@update')->name('products.update');
+		Route::delete('{product}', 'ProductController@delete')->name('products.delete');
+	});
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
-	Route::group(['prefix' => 'articles'], function(){
-	Route::get('news', 'ArticleController@newIndex');
-	Route::get('promotions', 'ArticleController@promotionIndex');
-	Route::post('/', 'ArticleController@store');
-	Route::get('news/create', 'ArticleController@newCreate');
-	Route::get('promotions/create', 'ArticleController@promotionCreate');
-	Route::get('edit/{article}', 'ArticleController@edit');
-	Route::put('{article}', 'ArticleController@update');
-	Route::delete('{article}', 'ArticleController@delete');
-	});	
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+	Route::group(['prefix' => 'articles'], function () {
+		Route::get('news', 'ArticleController@newIndex')->name('articles.newIndex');
+		Route::get('promotions', 'ArticleController@promotionIndex')->name('articles.promotionIndex');
+		Route::post('/', 'ArticleController@store');
+		Route::get('news/create', 'ArticleController@newCreate');
+		Route::get('promotions/create', 'ArticleController@promotionCreate');
+		Route::get('edit/{article}', 'ArticleController@edit');
+		Route::put('{article}', 'ArticleController@update');
+		Route::delete('{article}', 'ArticleController@delete');
+	});
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
-	Route::group(['prefix' => 'sliders'], function(){
-	Route::get('/', function(){
-		return view('slider.index');
-	})->name('sliders.index');
-	Route::post('/', 'SliderController@store')->name('sliders.store');
-	Route::get('create', 'SliderController@create')->name('sliders.create');
-	Route::get('{slider}', 'SliderController@show')->name('sliders.show');
-	Route::get('edit/{slider}', 'SliderController@edit')->name('sliders.edit');
-	Route::put('{slider}', 'SliderController@update')->name('sliders.update');
-	Route::delete('{slider}', 'SliderController@delete')->name('sliders.delete');
-	});	
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+	Route::group(['prefix' => 'sliders'], function () {
+		Route::get('/', function () {
+			return view('slider.index');
+		})->name('sliders.index');
+		Route::post('/', 'SliderController@store')->name('sliders.store');
+		Route::get('create', 'SliderController@create')->name('sliders.create');
+		Route::get('{slider}', 'SliderController@show')->name('sliders.show');
+		Route::get('edit/{slider}', 'SliderController@edit')->name('sliders.edit');
+		Route::put('{slider}', 'SliderController@update')->name('sliders.update');
+		Route::delete('{slider}', 'SliderController@delete')->name('sliders.delete');
+	});
 });
 
 
---
+++ storage/framework/views/3116ac206e0a08cef3fec15be4e2d0dbac369ee6.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(config('app.locale')); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <?php echo SEOMeta::generate(true); ?>
+
+    <?php echo OpenGraph::generate(); ?>
+
+    <?php echo Twitter::generate(); ?>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+    <!-- Styles -->
+
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>;
+    </script>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header col-md-offset-1">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
+                        <?php echo e(config('app.name', 'Laravel')); ?>
+
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+
+                        <li><a href="<?php echo e(route('articles.new')); ?>">最新消息</a></li>
+                        <li><a href="<?php echo e(route('articles.promotion')); ?>">口碑分享</a></li>
+                        <li><a href="<?php echo e(route('products.list')); ?>">最新車款</a></li>
+                        <li><a href="<?php echo e(route('articles.promotion')); ?>">關於我們</a></li>
+                        <li><a href="<?php echo e(route('articles.promotion')); ?>">聯絡我們</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <?php echo $__env->make('layouts.message', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+        <?php echo $__env->yieldContent('content'); ?>
+    </div>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html>
